bd8883135d61530e82de5bc3161185df79a56372
sirkrz <sirkrz@gmail.com>
2015-10-12 13:34:00 +0000

    string sum calculator

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,4 +1,21 @@
 
-public class StringCalculator {
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
+public class StringCalculator {
+	public static void main(String[] args) {
+		
+		String numbers = "";
+		int sum = Add(numbers);
+		StdOut.println(sum);
+	}
+	
+	public static int Add(String numbers){
+		int number = 0;
+		while(!StdIn.isEmpty()){
+	           numbers = StdIn.readString();
+	           number += Integer.parseInt(numbers);
+	     }
+		 return number;
+	}
 }
