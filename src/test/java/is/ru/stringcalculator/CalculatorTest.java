24a6e5a8c9d02b47190e87bf446f6accdd0b2a73
sirkrz <sir.krz@gmail.com>
2015-10-15 10:13:56 +0000

    test change

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+      org.junit.runner.JUnitCore.main("test.java.is.ru.stringcalculator.CalculatorTest");
     }
 
 	@Test
